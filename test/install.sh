344ad33b500a749fe00d2e0aaf50630823a9967d
Ondřej Fibich <fibich@freenetis.org>
2017-11-15 17:46:31 +0100

    Travis CI - adds install test with Docker.

---
+++ test/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 1 ]; then
+	echo "usage: $0 DEB"
+	exit 2
+fi
+
+DEB="$1"
+UNIT=/etc/init.d/freenetis-monitoring
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Install deb package"
+apt-get update -q
+apt install "$DEB"
+
+echo "Daemon test"
+$UNIT version
+$UNIT status | grep "FreenetIS monitor daemon is not running."
+$UNIT start | grep "Starting FreenetIS monitor daemon: OK"
+$UNIT status | grep "FreenetIS monitor daemon is running."
+$UNIT stop | grep "Stopping FreenetIS monitor daemon: OK"
+$UNIT status | grep "FreenetIS monitor daemon is not running."
